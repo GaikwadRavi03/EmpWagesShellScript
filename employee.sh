9445d6a772334fdc86531c27844ec6d103aa78d3
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-09-09 16:17:23 +0530

    Added code to check employee is presnt or not.

--- employee.sh
+++ employee.sh
@@ -1,1 +1,9 @@
-echo "welcome to employee wage game..."
+#!/bin/bash -x
+randomCheck=$((RANDOM%2))
+
+if [ $randomCheck -eq 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
